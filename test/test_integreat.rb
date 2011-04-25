288764d19133010272e98bbe97cbd1828aa8ee30
Matti Paksula <matti.paksula@iki.fi>
2011-04-25 23:39:36 +0300

    Megatest

--- test/test_integreat.rb
+++ test/test_integreat.rb
@@ -1,7 +1,41 @@
 require 'helper'
 
 class TestIntegreat < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+
+  should "Test with one megatest" do
+
+    Integreat "setups" do
+      
+      Setup "lolcat" do
+        @cat = "lol"
+      end
+      
+    end
+    
+    Integreat "Kissat koiria asdsfkokfdfdl" do
+      
+      Use "lolcat"
+      
+      Test "Cat is lol" do
+        
+        Step "lol is to be found" do
+          assert(@cat, "lol")
+        end
+      end
+      
+    end
+    
+    Integreat "Different context" do
+    
+      Test "Cat is not lol" do
+        
+        Step "not lol" do
+          assert(nil, @cat)
+        end
+      end
+    end
+    
+    assert(true)
   end
 end
